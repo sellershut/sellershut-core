c5fd79cd846c13c07bd9b283fafd362a0b4fce91
rtkay123 <dev@kanjala.com>
2024-05-04 14:14:57 +0200

    add: crate lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(
+    missing_docs,
+    rustdoc::broken_intra_doc_links,
+    missing_debug_implementations
+)]
+
+//! sellershut API utilities
+
 #[cfg(any(feature = "categories", feature = "users"))]
 #[cfg_attr(docsrs, doc(cfg(any(feature = "categories", feature = "users"))))]
+/// Shared utilities
 pub mod common;
 
 #[cfg(feature = "categories")]
 #[cfg_attr(docsrs, doc(cfg(feature = "categories")))]
+/// Categories API utilities
 pub mod category;
 
 #[cfg(feature = "users")]
 #[cfg_attr(docsrs, doc(cfg(feature = "users")))]
+/// Users API utilities
 pub mod user;
 
 #[cfg(feature = "users")]
 #[cfg_attr(docsrs, doc(cfg(feature = "users")))]
+/// Oauth utilities
 pub mod oauth;
